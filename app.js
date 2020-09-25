5a4c389fdc0e9f34592b14b96685238f6b56acc4
EphafroditeMin <vhf201@naver.com>
2020-09-25 22:50:14 +0900

    added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 
-canvas.width = 800;
-canvas.height = 1000;
+canvas.width = 750;
+canvas.height = 900;
 
 ctx.strokeStyle = "#040404";
 ctx.lineWidth = 2.5;
